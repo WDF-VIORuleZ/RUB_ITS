a20df2f577dd860e80a8e1d01452b2275ea77e71
Leon Groß <leon_gross@protonmail.ch>
2019-05-18 22:07:16 +0200

    seperated modules

--- sem_2/EFK2/modules/eea_module.py
+++ sem_2/EFK2/modules/eea_module.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+# -*- coding:utf-8 -*-
 
 import argparse
 
@@ -9,8 +10,8 @@ def eea(mod, el): #Condition: gcd(m,x)=1
     q = []
     s = [1]
     t = [0]
-
     ctr = 0
+    
     while(x[ctr] != 0):
         q.insert(0,int(m[ctr]/x[ctr]))
         m.append(x[ctr])
@@ -25,16 +26,8 @@ def eea(mod, el): #Condition: gcd(m,x)=1
     q = q[::-1]
     s = s[::-1]
     t = t[::-1]
-    
-    print(f"{'m':4}|{'x':4}|{'q':4}||{'s':4}|{'t':4}|") #limits us on 4 digit numbers
-    print("--------------------------")
-    for i in range(len(q)):
-        print(f"{m[i]:<4}|{x[i]:<4}|{q[i]:<4}||{s[i]:<4}|{t[i]:<4}|")
-    
     test = s[0] * m[0] + t[0] * x[0] 
-    print(f"\nTest: {s[0]} * {m[0]} + {t[0]} * {x[0]} = {test}")
-    
-    print(f"\n{el}⁻¹ = {t[0]} mod {mod}")
+    return s
 
 
 def eea_latex(mod,el):
@@ -72,18 +65,3 @@ def eea_latex(mod,el):
     print("\\end{tabular}")
     print("\\end{center}")
     
-
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='Extended Euclidean algorithm')
-    parser.add_argument('el', metavar='E', type=int, help='element to calculate the modular multiplicative inverse of')
-    parser.add_argument('mod', metavar='M', type=int, help='modul of element')
-    parser.add_argument("-l", "--latex", help="Print output in LaTeX", action='store_true')
-    args = parser.parse_args()
-
-    if(args.latex):
-        eea_latex(args.mod,args.el)
-    else:
-        eea(args.mod,args.el)
